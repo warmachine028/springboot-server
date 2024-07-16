52938b7f482ac896804664b51ab1140d8f96c462
Pritam Kundu <warmachine028@outlook.com>
2024-07-17 01:44:05 +0530

    Update and rename take-snapshot.js to take-snapshot.mjs

--- .github/take-snapshot.mjs
+++ .github/take-snapshot.mjs
@@ -1,13 +1,14 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+import puppeteer from 'puppeteer';
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 const takeSnapShot = async () => {
   const url = 'https://warmachine028.github.io/springboot-server/client';  // Replace with your actual deployed URL
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.screenshot({ path: '.github/preview.png' });
+  await page.goto(url);
+  await sleep(3000);
+  await page.screenshot({ path: '.github/preview.png', fullPage: true, captureBeyondViewport: true });
   await browser.close();
 };
 
