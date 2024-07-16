80261dcc92347a136415fa3967f0f4e6f8a5a92d
Pritam Kundu <warmachine028@outlook.com>
2024-07-16 20:37:28 +0530

    Create take-snapshot.js

---
+++ .github/take-snapshot.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const takeSnapShot = async () => {
+  const url = 'https://warmachine028.github.io/springboot-server/client';  // Replace with your actual deployed URL
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.screenshot({ path: 'snapshot.png' });
+  await browser.close();
+};
+
+await takeSnapShot();
