8d517eb6d2762d45ba8b95a3353055adf331dfde
Pritam Kundu <warmachine028@outlook.com>
2024-07-16 21:37:16 +0530

    Create update-preview.sh

---
+++ .github/update-preview.sh
@@ -0,0 +1,4 @@
+SNAPSHOT_MARKDOWN="![Snapshot](snapshot.png)"
+README_CONTENT=$(cat README.md)
+UPDATED_README=$(echo "$README_CONTENT" | sed "/## Preview/a $SNAPSHOT_MARKDOWN")
+echo "$UPDATED_README" > README.md
